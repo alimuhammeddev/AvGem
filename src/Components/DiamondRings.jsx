b854fc7c536dcaf19b7a2b41cf9382f9b9beb4de
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2025-05-22 21:32:21 +0100

    reduced text size

--- src/Components/DiamondRings.jsx
+++ src/Components/DiamondRings.jsx
@@ -4,7 +4,7 @@ const DiamondRings = () => {
   return (
     <section>
         <div className='max-w-7xl mx-auto'>
-            <h1 className='text-[#97877a] lg:text-4xl text-2xl font-medium mt-10 text-center'>Customizable Rough Diamond Rings</h1>
+            <h1 className='text-[#97877a] lg:text-4xl text-xl font-medium mt-10 text-center'>Customizable Rough Diamond Rings</h1>
         </div>
     </section>
   )

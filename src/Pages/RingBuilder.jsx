ee8df963eae2d886af7fa93c2775973359d12b96
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2025-06-07 12:58:58 +0100

    commit

--- src/Pages/RingBuilder.jsx
+++ src/Pages/RingBuilder.jsx
@@ -12,33 +12,33 @@ const RingBuilder = () => {
                 <div>
                     <h1 className='lg:text-3xl text-center font-semibold'>Your Custom Ring</h1>
                     <div class="flex items-center justify-between px-4 py-8 space-x-2 sm:space-x-4">
-  
-  <div class="flex flex-col items-center flex-1 relative">
-    <div class="w-10 h-10 flex items-center justify-center rounded-full bg-blue-600 text-white text-xl font-bold z-10">1</div>
-    <div class="mt-2 text-center text-sm">Step One</div>
-    <div class="absolute top-5 left-1/2 w-full h-0.5 bg-blue-300 -z-0"></div>
-  </div>
 
-  
-  <div class="flex flex-col items-center flex-1 relative">
-    <div class="w-10 h-10 flex items-center justify-center rounded-full bg-blue-600 text-white text-xl font-bold z-10">2</div>
-    <div class="mt-2 text-center text-sm">Step Two</div>
-    <div class="absolute top-5 left-0 lg:w-[500px] w-[100px] h-0.5 bg-blue-300 -z-0"></div>
-  </div>
+                        <div class="flex flex-col items-center flex-1 relative">
+                            <div class="w-10 h-10 flex items-center justify-center rounded-full bg-blue-600 text-white text-xl font-bold z-10">1</div>
+                            <div class="mt-2 text-center text-sm">Choose your stone type</div>
+                            <div class="absolute top-5 left-1/2 w-full h-0.5 bg-blue-300 -z-0"></div>
+                        </div>
 
-  
-  <div class="flex flex-col items-center flex-1 relative">
-    <div class="w-10 h-10 flex items-center justify-center rounded-full bg-blue-600 text-white text-xl font-bold z-10">3</div>
-    <div class="mt-2 text-center text-sm">Step Three</div>
-    <div class="absolute top-5 left-0 lg:w-[450px] w-[120px] h-0.5 bg-blue-300 -z-0"></div>
-  </div>
 
- 
-  <div class="flex flex-col items-center flex-1 relative">
-    <div class="w-10 h-10 flex items-center justify-center rounded-full bg-blue-600 text-white text-xl font-bold z-10">4</div>
-    <div class="mt-2 text-center text-sm">Step Four</div>
-  </div>
-</div>
+                        <div class="flex flex-col items-center flex-1 relative">
+                            <div class="w-10 h-10 flex items-center justify-center rounded-full bg-blue-600 text-white text-xl font-bold z-10">2</div>
+                            <div class="mt-2 text-center text-sm">Select your center stone</div>
+                            <div class="absolute top-5 left-0 lg:w-[500px] w-[100px] h-0.5 bg-blue-300 -z-0"></div>
+                        </div>
+
+
+                        <div class="flex flex-col items-center flex-1 relative">
+                            <div class="w-10 h-10 flex items-center justify-center rounded-full bg-blue-600 text-white text-xl font-bold z-10">3</div>
+                            <div class="mt-2 text-center text-sm">Select your ring setting</div>
+                            <div class="absolute top-5 left-0 lg:w-[450px] w-[120px] h-0.5 bg-blue-300 -z-0"></div>
+                        </div>
+
+
+                        <div class="flex flex-col items-center flex-1 relative">
+                            <div class="w-10 h-10 flex items-center justify-center rounded-full bg-blue-600 text-white text-xl font-bold z-10">4</div>
+                            <div class="mt-2 text-center text-sm">Your custom ring is ready!</div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
